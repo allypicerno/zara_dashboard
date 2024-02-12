8147b505c740edbd884b22ea3e4904106ea65966
ariellegberg <arielle_greenberg@yahoo.com>
2024-02-12 14:52:45 -0500

    Added checklist

--- dashboard_api.py
+++ dashboard_api.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output
 import plotly.graph_objects as go
-# pip install xlrd
+#pip install xlrd
 
 df = pd.read_excel('fashion_data_2018_2022.xls')
 df = df[['price', 'year_of_sale', 'category', 'average_rating', 'age_group', 'gender']].dropna()
@@ -21,16 +21,25 @@ app.layout = html.Div([
         value=df['category'].unique()[0],
         clearable=True
     ),
-    dcc.Graph(id='graph')
-])
+    dcc.Graph(id='graph'),
+    html.Br(),
+    html.P('Filter by age group: '),
+    dcc.Checklist(
+        id='age-group-checklist',
+        options=[{'label': ag, 'value': ag} for ag in df['age_group'].unique()],
+        value=df['age_group'].unique(),
+        inline=True)])
 
 # Define callback to update the graph
 @app.callback(
     Output('graph', 'figure'),
-    [Input('dropdown', 'value')]
+    [Input('dropdown', 'value'),
+     Input('age-group-checklist', 'value')]
 )
-def update_graph(selected_category):
+def update_graph(selected_category, selected_age_groups):
     filtered_data = df[df['category'] == selected_category]
+    if selected_age_groups:
+        filtered_data = filtered_data[filtered_data['age_group'].isin(selected_age_groups)]
     data = filtered_data.groupby('year_of_sale')['price'].mean().reset_index()
 
     years = data['year_of_sale']
@@ -41,7 +50,7 @@ def update_graph(selected_category):
     fig = go.Figure(data=go.Scatter(x=years, y=prices, mode='markers+lines', connectgaps=True))
 
     # Update layout
-    fig.update_layout(title=f'Price over Years for Category {selected_category}',
+    fig.update_layout(title=f'Average {selected_category} Price over the Years',
                       xaxis_title='Year',
                       yaxis_title='Price',
                       xaxis=dict(tickmode='linear'))
